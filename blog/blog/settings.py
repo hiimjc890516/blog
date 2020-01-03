9eaa3a1149cf3ee5fa331c1b10f7837324791b6e
hiimjc890516 <hiimjc890516@gmail.com>
2020-01-03 16:08:00 +0800

    ch11 finish

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'article',
     'main',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'account.User'
